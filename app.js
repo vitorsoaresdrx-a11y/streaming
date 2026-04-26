376833a743557d2a44a5886e9fac5fd849e1ad84
AllcanceAI <gabrielalvestrabalhotrader@gmail.com>
2026-04-26 02:28:13 -0300

    Switch to AutoEmbed Player endpoint with Portuguese flag

--- app.js
+++ app.js
@@ -216,11 +216,11 @@ async function playContent() {
     let embedUrl = "";
 
     if (type === 'movie') {
-        embedUrl = `https://autoembed.to/movie/tmdb/${currentMovieId}?server=1`;
+        embedUrl = `https://player.autoembed.to/movie/${currentMovieId}?lang=pt`;
     } else {
         const season = document.getElementById('season-select').value || 1;
         const episode = window.currentEpisodeNumber || 1;
-        embedUrl = `https://autoembed.to/tv/tmdb/${currentMovieId}-${season}-${episode}?server=1`;
+        embedUrl = `https://player.autoembed.to/tv/${currentMovieId}/${season}/${episode}?lang=pt`;
     }
 
     videoContainer.innerHTML = `<iframe src="${embedUrl}" allow="autoplay; encrypted-media; fullscreen" allowfullscreen></iframe>`;
